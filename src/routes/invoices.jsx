d1458f59ba46912fdb263632d3f8b3e1c355d83d
TravisWongX <travis.x.wong@gmail.com>
2022-03-13 21:16:51 +0800

    useSearchParams

--- src/routes/invoices.jsx
+++ src/routes/invoices.jsx
@@ -1,15 +1,34 @@
-import { Link, NavLink, Outlet } from "react-router-dom"
+import { Link, NavLink, Outlet, useSearchParams } from "react-router-dom"
 import { getInvoices } from "../data"
 
 export default function Invoices() {
   let invoices = getInvoices()
+  let [searchParams, setSearchParams] = useSearchParams()
+
   return (
     <div style={{display: "flex"}}>
       <nav style={{
         borderRight: "solid 1px",
         padding: "1rem"
       }}>
-        {invoices.map((invoice) => (
+        <input 
+          value={searchParams.get("xxx") || ""}
+          onChange={(event)=> {
+            let xxx = event.target.value;
+            if(xxx) {
+              setSearchParams({xxx, b: 111});
+            } else {
+              setSearchParams({})
+            }
+          }} />
+        {invoices
+        .filter((invoice) => {
+          let xxx = searchParams.get("xxx")
+          if (!xxx) return true;
+          let name = invoice.name.toLowerCase()
+          return name.startsWith(xxx.toLowerCase())
+        })
+        .map((invoice) => (
           <NavLink 
             style={({isActive}) => (
               {
